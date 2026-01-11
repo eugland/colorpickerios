eaf16e85d67c99d36ad2df61adeccf0140be8705
Eugene <eugene.r.w.12@gmail.com>
2026-01-10 17:49:59 -0800

    Initial Commit

---
+++ color/colorApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorApp.swift
+//  color
+//
+//  Created by eugland on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct colorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
